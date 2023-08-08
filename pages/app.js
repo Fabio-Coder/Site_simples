a7e05d6140d2db2d2b60f13b6c0645aeca1315b1
Fábio Santos <0xfab1015@gmail.com>
2023-08-08 00:37:55 +0000

    Bug do botao resolvido

--- pages/app.js
+++ pages/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const switcher = document.querySelector(".btn");
+const switcher = document.querySelector(".button");
 switcher.addEventListener("click", function () {
   document.body.classList.toggle("dark-theme");
   var className = document.body.className;
